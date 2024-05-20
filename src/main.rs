e844e382dc1814aaa1426315d3def86d43458fbb
Shubhamai <shubham.aiengineer@gmail.com>
2024-05-20 13:03:58 +0530

    cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::{Arg, Command};
+use colored::*;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("silo")
+        .bin_name("silo")
+        .version(env!("CARGO_PKG_VERSION"))
+        .about("[WIP] Build and deploy containers in seconds")
+        .subcommand_required(true)
+        .subcommand(
+            Command::new("run").about("Run a container").arg(
+                Arg::new("CONTAINER")
+                    .help("The path of container to run")
+                    .required(true)
+                    .index(1),
+            ),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("run", sub_m)) => {
+            let script = sub_m.get_one::<String>("CONTAINER").unwrap();
+            println!("{}", format!("Running {}...", script).green());
+        }
+        _ => {
+            println!("{}", "No valid subcommand was used".red());
+        }
+    }
 }
